645148291d51e90c0f08b6c5906136975faf75c5
somegitking <ironvegapentech@gmail.com>
2021-11-12 16:21:37 +0900

    Initial commit

---
+++ js/lotto.js
@@ -0,0 +1,20 @@
+console.log("lotto...................")
+console.log("lotto...................")
+console.log("lotto...................")
+console.log("lotto...................")
+
+const btn = document.getElementById("btn")
+
+console.log(btn)
+
+btn.addEventListener("click",(event) => {
+    console.log(event)
+},false)
+
+//이벤트 관련 false 의미 확인헤 보기
+
+
+// 콜백함수 스타일
+// 1. =>{}
+// 2. ,function()
+// 3. , doA
